c4bc67d5653f4a274920ac7bfe2b7c91ad8b96d0
strikerOf <luis.st.juarez@gmail.com>
2025-01-03 18:32:46 -0600

    prod

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { outDir: 'dist', rollupOptions: { output: { manualChunks: undefined 
+    // Esto es opcional, pero puede ayudar en algunos casos 
+    } } }
 })
